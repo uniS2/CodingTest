9dbe1b47071f1001e6784d0971eccf8a440cece7
uniS2 <134567469+uniS2@users.noreply.github.com>
2023-07-04 22:30:28 +0900

    [level 0] Title: 두 수의 차, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120803. 두 수의 차/두 수의 차.js
@@ -0,0 +1,8 @@
+function solution(num1, num2) {
+    if(num1 >= -50000 && num1 <= 50000) {
+        if(num2 >= -50000 && num1 <= 50000) {
+            let answer = num1 - num2;
+            return answer;
+        }
+    }
+}
