569243c699defca920200e5a99835b7fac426c65
uniS2 <134567469+uniS2@users.noreply.github.com>
2023-07-10 11:31:43 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,11 +1,5 @@
 function solution(n) {
     let arr = [];
-    for(let i = 1; i <= n; i++){
-        if(i % 2 === 0){
-            continue;
-        } else {
-            arr.push(i);  
-        }
-    }
+    for(let i = 1; i <= n; i+=2) arr.push(i);
     return arr
 }
