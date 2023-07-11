31816c23fa3d828e75c0b351aacc1592f2d5c6e7
uniS2 <134567469+uniS2@users.noreply.github.com>
2023-07-12 02:20:15 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
+++ 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
@@ -1,8 +1,24 @@
 function solution(n) {
-    let str = (n+'').split('');
+    // 문자풀이
+    let str = (n+'').split(''); // 배열
     let arr = [];
-    for (let i = str.length-1; i >= 0; i--){
-        arr.push(+str.slice(i,i+1));
+    
+    // 1. for문
+    /* for (let i = str.length-1; i >= 0; i--){
+        arr.push(+str[i]);
+        arr.unshift(+str[i]); // 이 방식은 for문 오름차순을 그대로 사용할 수 있음
     }
+    return arr; */
+    
+    // 2. reverse(), map()
+    // Array.prototype.reverse(): 배열의 순서 반전
+    // Array.prototype.map(): 배열 내 각각 보든 요소에 대해 주어진 함수를 호출한 결과를 모아 새로운 배열을 반환
+    // return str.reverse().map(a => parseInt(a));
+    
+    // 3. 숫자풀이 - do, while
+    do {
+        arr.push(n%10);
+        n = Math.floor(n/10);
+    } while (n>0);
     return arr;
 }
