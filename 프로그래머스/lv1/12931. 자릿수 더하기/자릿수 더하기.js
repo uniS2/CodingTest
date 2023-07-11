482af8142eaa58dcdbe980f0cc5bb8897637c097
uniS2 <134567469+uniS2@users.noreply.github.com>
2023-07-12 01:39:29 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,13 +1,26 @@
 function solution(n)
 {
-    // 나의 풀이: 문자열 변환 후 slice() 메서드 이용
-    // let str = String(n);
-    // let sum = 0;
-    // for(let i = 1; i <= n; i++){
-    //     sum += Number(str.slice(i-1, i));
-    // }
-    // return sum;
+    // 문자 풀이
+    // 1. 나의 풀이: 문자열 변환 후 slice() 메서드 이용
+    /*
+    let str = String(n);
+    let sum = 0;
+    for(let i = 1; i <= n; i++){
+        sum += Number(str.slice(i-1, i));
+    }
+    return sum;
+    */
     
-    // split() (배열)과 reduce() 메서드 이용
+    // 2. split() (배열)과 reduce() 메서드 이용
     return (n+'').split('').reduce((acc, cur) => (acc + parseInt(cur)), 0)
+    
+    // 3. 숫자 풀이
+    /*
+    do {
+        sum += n%10;
+        n = Math.floor(n/10);
+    } while(n > 0);
+    
+    return sum;
+    */
 }
