7164e45016831b27ad2056ebd07d1c6b6d1b99a2
uniS2 <134567469+uniS2@users.noreply.github.com>
2023-07-18 01:02:25 +0900

    [unrated] Title: 홀짝 구분하기, Time: 43.09 ms, Memory: 32.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.js
+++ 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.js
@@ -10,9 +10,5 @@ rl.on('line', function (line) {
     input = line.split(' ');
 }).on('close', function () {
     n = Number(input[0]);
-    if(n % 2 === 0){
-        console.log (`${n} is even`)
-    } else {
-        console.log (`${n} is odd`)
-    }
+    (n % 2)?console.log (`${n} is odd`):console.log (`${n} is even`)
 });
