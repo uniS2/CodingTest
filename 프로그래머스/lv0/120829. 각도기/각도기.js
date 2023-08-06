97c1e5aa884fd3b602a3a95038da2ed2c707cec4
uniS2 <134567469+uniS2@users.noreply.github.com>
2023-08-07 01:56:33 +0900

    [level 0] Title: 각도기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.js
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -1,9 +1,3 @@
 function solution(angle) {
-    if(angle === 180){
-        return 4;
-    } else if (angle > 90){
-        return 3;
-    } else if (angle === 90){
-        return 2;
-    } else return 1;
+    return [0, 90, 91, 180].filter(item => angle >= item).length;
 }
