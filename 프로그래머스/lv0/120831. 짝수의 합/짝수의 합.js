19e81c3895a251c35a8c996187a7e7e9f09f3cee
uniS2 <134567469+uniS2@users.noreply.github.com>
2023-08-09 21:21:40 +0900

    [level 0] Title: 짝수의 합, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
@@ -1,7 +1,4 @@
 function solution(n) {
-    let sum = 0;
-    for(let i = 2; i <= n; i+=2){
-        sum += i;
-    }
-    return sum;
+    const half = Math.floor(n/2);
+    return half * (half + 1);
 }
