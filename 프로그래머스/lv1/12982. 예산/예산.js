decf00182b326f69fba569d2ee16096345ccf3f5
uniS2 <134567469+uniS2@users.noreply.github.com>
2023-08-29 02:35:16 +0900

    [level 1] Title: 예산, Time: 0.63 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.js
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -1,11 +1,10 @@
 function solution(d, budget) {
-    d.sort((a, b) => a - b);
     for(let i in d){
-        console.log(d.slice(0, +i + 1))
-        if (d.slice(0, +i + 1).reduce((a, b) => a + b, 0) === budget) {
+        let max = d.sort((a, b) => a - b).slice(0, +i + 1).reduce((a, b) => a + b, 0);
+        if (max === budget) {
             return +i + 1;
         } 
-        else if (d.slice(0, +i + 1).reduce((a, b) => a + b, 0) > budget) {
+        else if (max > budget) {
             return +i;
         }
     }
