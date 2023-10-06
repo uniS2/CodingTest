52777cc8c37b822617a1a33b93b52c4bb727bc3e
uniS2 <134567469+uniS2@users.noreply.github.com>
2023-10-06 11:19:33 +0900

    Update: 덧셈식 출력하기
    - 배열 구조 분해 할당 이용

--- 프로그래머스/Lv.0/181947. 덧셈식 출력하기/덧셈식 출력하기.js
+++ 프로그래머스/Lv.0/181947. 덧셈식 출력하기/덧셈식 출력하기.js
@@ -7,7 +7,7 @@ const rl = readline.createInterface({
 let input = [];
 
 rl.on('line', function (line) {
-    input = line.split(' ');
+    [a, b] = line.split(' ');
 }).on('close', function () {
-    console.log(`${input[0]} + ${input[1]} = ${Number(input[0]) + Number(input[1])}`);
+    console.log(`${a} + ${b} = ${Number(a) + Number(b)}`);
 });
