87dbce103e98210a5edea6bf11d63610966e9821
uniS2 <134567469+uniS2@users.noreply.github.com>
2023-10-14 00:08:16 +0900

    [level unrated] Title: 날짜 비교하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
@@ -1,3 +1,4 @@
 function solution(date1, date2) {
-     return date1.join('') - date2.join('') < 0 ? 1 : 0;
+    // return new Date(date1) < new Date(date2) ? 1 : 0;
+    return date1.join('') - date2.join('') < 0 ? 1 : 0;
 }
