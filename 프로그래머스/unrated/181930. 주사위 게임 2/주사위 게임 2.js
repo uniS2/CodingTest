ec86a80e529fc8b0b3d631b69b7c4b397dd2d108
uniS2 <134567469+uniS2@users.noreply.github.com>
2024-01-02 11:49:32 +0900

    [level unrated] Title: 주사위 게임 2, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,9 @@
+function solution(a, b, c) {
+    const sum = a + b + c;
+    const powTwo = Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2)
+    const powThree = Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3)
+    
+    if (a !== b && b !== c && c !== a) return sum;
+    else if (a === b && b === c) return sum * powTwo * powThree;
+    else return sum * powTwo;
+}
