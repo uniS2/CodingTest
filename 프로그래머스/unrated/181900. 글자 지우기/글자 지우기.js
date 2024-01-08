22166c35b08a325f778b523bd586b17a1fb7dc64
uniS2 <s2oie1010@gmail.com>
2024-01-09 00:17:39 +0900

    [level unrated] Title: 글자 지우기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.js
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.js
@@ -1,5 +1,3 @@
 function solution(my_string, indices) {
-    return [...my_string].filter((v, i) => {
-        if(!indices.includes(i)) return v;
-    }).reduce((a, b) => a + b, '')
+    return [...my_string].filter((v, i) => !indices.includes(i)).join('')
 }
