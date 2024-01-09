704d8493b28a20e2d730ece6ebfd9fb3ee50842b
uniS2 <s2oie1010@gmail.com>
2024-01-09 22:44:04 +0900

    Remove: 파일 삭제

--- 프로그래머스/unrated/120843. 공 던지기/공 던지기.js
+++
@@ -1,8 +0,0 @@
-function solution(numbers, k) {    
-    if (numbers.length % 2) numbers.push(...numbers);
-    
-    const arr = numbers.filter((i, v) => !(v % 2));
-    
-    if (k % arr.length) return arr[k % arr.length - 1];
-    else return arr[arr.length - 1];
-}
