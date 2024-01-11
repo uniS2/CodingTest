b0a24d581936f501ac0cca3654d5bdf9c1040acf
uniS2 <s2oie1010@gmail.com>
2024-01-11 23:26:32 +0900

    [Bronze V] Title: 사칙연산, Time: 128 ms, Memory: 9600 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+
+const [a, b] = fs.readFileSync(path, 'utf8').trim().split(' ');
+console.log(+a + +b);
+console.log(a - b);
+console.log(a * b);
+console.log(Math.floor((a / b)));
+console.log(a % b);
