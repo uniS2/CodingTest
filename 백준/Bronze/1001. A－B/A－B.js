2d7dca268f0d471fb23aaefee17bca0fa9797e3a
uniS2 <s2oie1010@gmail.com>
2024-01-11 23:10:21 +0900

    [Bronze V] Title: A-B, Time: 124 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.js
@@ -0,0 +1,4 @@
+const fs = require('fs');
+const path = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const [a, b] = fs.readFileSync(path, 'utf8').trim().split(' ');
+console.log(a - b)
