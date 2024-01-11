ac66a8a6c1fc32595630853706cfabc6cb3c4238
uniS2 <s2oie1010@gmail.com>
2024-01-11 22:28:31 +0900

    [level Lv.0] Title: 리스트 자르기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181897. 리스트 자르기/리스트 자르기.js
@@ -0,0 +1,7 @@
+function solution(n, slicer, num_list) {
+    const [a, b, c] = slicer;
+    return (n === 1) ? num_list.slice(0, b + 1)
+        : (n === 2) ? num_list.slice(a, )
+        : (n === 3) ? num_list.slice(a, b + 1)
+        : num_list.slice(a, b + 1).filter((_, i) => !(i % c));
+}
