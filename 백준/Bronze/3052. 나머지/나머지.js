b9fca75c4adf2126209fa73a61f9d14fcf34531f
uniS2 <s2oie1010@gmail.com>
2024-01-19 15:03:30 +0900

    [Bronze II] Title: 나머지, Time: 124 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.js
+++ 백준/Bronze/3052. 나머지/나머지.js
@@ -1,4 +1,7 @@
 let input = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n').map(Number);
 
-const a = new Set(input.map(n => n % 42))
-return console.log(a.size);
+let result = {};
+input.map(n => n % 42).forEach(i => {
+  result[i] = 1;
+})
+return console.log(Object.entries(result).length);
