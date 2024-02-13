04b34fc40ba738247bf01d92088ed1d7d08b9a91
uniS2 <s2oie1010@gmail.com>
2024-02-13 15:50:40 +0900

    [Bronze III] Title: 최댓값, Time: 116 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.js
+++ 백준/Bronze/2566. 최댓값/최댓값.js
@@ -3,6 +3,6 @@ let input = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n');
 input = input.map(i => i.split(' ').map(Number));
 const max = Math.max(...input.flat());
 
-input.forEach((v, i) => {
-  if (v.includes(max)) return console.log(`${max}\n${i + 1} ${v.indexOf(max) + 1}`);
-})
+for (let i = 0; i < 9; i++) {
+  if (input[i].includes(max)) return console.log(`${max}\n${i + 1} ${input[i].indexOf(max) + 1}`);
+}
