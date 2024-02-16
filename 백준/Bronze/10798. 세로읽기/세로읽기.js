96facf6b7142b852f2794dbb6775223162ba7348
uniS2 <s2oie1010@gmail.com>
2024-02-16 17:06:33 +0900

    [Bronze I] Title: 세로읽기, Time: 136 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.js
+++ 백준/Bronze/10798. 세로읽기/세로읽기.js
@@ -1,11 +1,12 @@
 const input = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n').map(i => i.trim());
 
-let result = '';
-const length = [...input].sort((a, b) => b.length - a.length)[0].length;
+const length = Math.max(...input.map(i => i.length));
 
+let result = '';
 for (let i = 0; i < length; i++) {
   for (let j = 0; j < input.length; j++) {
-    result += input[j][i] ? input[j][i] : '';
+    result += input[j][i] || '';
   }
 }
+
 console.log(result);
