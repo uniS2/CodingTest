8fed7a9508df8d6226c5301963cd2326265f8b5b
uniS2 <s2oie1010@gmail.com>
2024-02-17 23:16:37 +0900

    [Bronze III] Title: 약수 구하기, Time: 136 ms, Memory: 9596 KB -BaekjoonHub

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.js
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.js
@@ -1,10 +1,10 @@
-const input = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split(' ');
+const [p, q] = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split(' ').map(Number);
 
 let result = [];
-for (i = 1; i <= input[0]; i++) {
-  if (input[0] % i === 0) result.push(i);
-  if (result.length == input[1]) {
-    return console.log(result[input[1] - 1])
+for (i = 1; i <= p; i++) {
+  if (p % i === 0) result.push(i);
+  if (result.length == q) {
+    return console.log(result[q - 1])
   }
 }
 console.log(0);
