38f4e9f882c9f3decad5c91c1cada78b57537426
uniS2 <s2oie1010@gmail.com>
2024-02-19 22:18:34 +0900

    [Bronze V] Title: N 찍기, Time: 216 ms, Memory: 24328 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.js
+++ 백준/Bronze/2741. N 찍기/N 찍기.js
@@ -1,6 +1,6 @@
 const N = require('fs').readFileSync('/dev/stdin', 'utf8').trim();
 let result = [];
 for (let i = 1; i <= N; i++) {
-  result.push(i);
+  result[i - 1] = i;
 }
 console.log(result.join('\n'));
