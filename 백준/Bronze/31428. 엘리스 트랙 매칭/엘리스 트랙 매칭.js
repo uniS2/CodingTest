36bd6b121ff2bd20a58c0730c0ce4b9665965da5
uniS2 <s2oie1010@gmail.com>
2024-02-19 22:06:45 +0900

    [Bronze IV] Title: 엘리스 트랙 매칭, Time: 124 ms, Memory: 9428 KB -BaekjoonHub

--- 백준/Bronze/31428. 엘리스 트랙 매칭/엘리스 트랙 매칭.js
+++ 백준/Bronze/31428. 엘리스 트랙 매칭/엘리스 트랙 매칭.js
@@ -1,8 +1,8 @@
 let [N, info, track] = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n');
 
-info = info.split(' ');
+info = info.trim().split(' ');
 let result = 0;
-for (let i = N - 1; i >= 0; i--) {
-  if (info[i].trim() == track) result++;
+for (let i = 0; i < N; i++) {
+  if (info[i] == track) result++;
 }
 console.log(result);
