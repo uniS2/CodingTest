5f17f69b95b3a0b8fa458593e3603f87e7c5520a
uniS2 <s2oie1010@gmail.com>
2024-02-24 18:03:40 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -1,12 +1,10 @@
 function solution(a, b) {
-    let max = 0;
+    let max = 1;
     for (let i = 2; i <= Math.max(a, b); i++){
         if(!(a % i) && !(b % i)) max = i;
     }
-    if (max) b /= max;
-    while(!(b % 2) || !(b % 5)) {
-        if (!(b % 2)) b /= 2;
-        if (!(b % 5)) b /= 5;
-    }
+    b /= max;
+    while (!(b % 2)) b /= 2;
+    while (!(b % 5)) b /= 5;
     return b == 1 ? 1 : 2;
 }
