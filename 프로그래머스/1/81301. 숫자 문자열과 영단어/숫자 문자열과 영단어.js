483c1c8c0986521f49228e24aeb95f53f7c18651
uniS2 <s2oie1010@gmail.com>
2024-03-01 23:08:33 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,8 +1,7 @@
 function solution(s) {
-    const data = {'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4,
-                  'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
-    Object.keys(data).forEach((word) => {
-        if(s.includes(word)) s = s.replaceAll(word, data[word]);
+    const word = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'];
+    word.forEach((w, index) => {
+        if(s.includes(w)) s = s.replaceAll(w, index);
     })
     return Number(s);
 }
