0cde19926f299c88d9a88c826397df20271e907e
uniS2 <s2oie1010@gmail.com>
2024-03-04 00:04:57 +0900

    [Bronze I] Title: 소인수분해, Time: 424 ms, Memory: 9872 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.js
+++ 백준/Bronze/11653. 소인수분해/소인수분해.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 let N = fs.readFileSync('/dev/stdin', 'utf8').trim();
 
-let result = [];
+let result = '';
 let i = 2;
 
 while (N != 1) {
   if (!(N % i)) {
-    result.push(i);
+    result += `${i}\n`
     N /= i;
   } else i++;
 }
 
-console.log(result.join('\n'));
+console.log(result);
