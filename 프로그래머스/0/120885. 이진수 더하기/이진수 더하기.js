6d025b1e461286123826e9f7f26582b582b7b512
uniS2 <s2oie1010@gmail.com>
2024-03-14 16:23:41 +0900

    [level 0] Title: 이진수 더하기, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.js
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.js
@@ -1,3 +1,27 @@
 function solution(bin1, bin2) {
-    return (parseInt(bin1, 2) + parseInt(bin2, 2)).toString(2);
+    bin1 = [...bin1].reverse().map(b => Boolean(Number(b)));
+    bin2 = [...bin2].reverse().map(b => Boolean(Number(b)));
+    let result = [];
+    
+    for(let index = 0; index < Math.max(bin1.length, bin2.length); index++){
+        if (!result[index]){
+            if (bin1[index] && bin2[index]){
+                result[index] = false;
+                result[index + 1] = true;
+            } else if (bin1[index] || bin2[index]) {
+                result[index] = true;
+            } else {
+                result[index] = false;
+            }
+        } else if (bin1[index] || bin2[index]) {
+            if (bin1[index] && bin2[index]){
+                result[index + 1] = true;
+            } else if (bin1[index] || bin2[index]) {
+                result[index] = false;
+                result[index + 1] = true;
+            }
+        }
+    }
+    
+    return result.reverse().map(Number).join('');
 }
