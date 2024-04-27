8e5bf130966dada6a9b11470dcdcf16ffba88c4e
uniS2 <s2oie1010@gmail.com>
2024-04-27 13:57:26 +0900

    [level 2] Title: 전화번호 목록, Time: 406.31 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -9,6 +9,6 @@ function solution(phone_book) {
     for(let [book, phone] of hashMap) {
         const next = hashMap.get(book + 1);
         if (!next) return true;
-        else if (next.startsWith(phone)) return false;
+        else if (next.indexOf(phone) === 0) return false;
     }
 }
