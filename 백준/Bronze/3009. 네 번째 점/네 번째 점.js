5ee04bfd1d008dec23bb7ac3f3043e568168f518
uniS2 <s2oie1010@gmail.com>
2024-07-16 22:45:15 +0900

    [Bronze III] Title: 네 번째 점, Time: 96 ms, Memory: 9320 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.js
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.js
@@ -1,20 +1,14 @@
 const fs = require('fs');
-const path = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync('/dev/stdin', 'utf8').trim().split('\n').map(i => i.split(' '));
 
-const input = fs.readFileSync(path, 'utf8').trim().split('\n').map(i => i.split(' '));
+function solution(arr) {
+  const x = arr.map(v => +v[0]);
+  const y = arr.map(v => +v[1]);
 
-let x = input.map(i => +i[0]).sort();
-let y = input.map(i => +i[1]).sort();
-let answer = `${x[0]} `;
+  let answer = x.filter((v) => x.indexOf(v) === x.lastIndexOf(v))[0] + ' ';
+  answer += y.filter((v) => y.indexOf(v) === y.lastIndexOf(v))[0];
 
-if (x[0] == x[1]) {
-  answer = `${x[2]} `;
+  return answer;
 }
 
-if (y[0] === y[1]) {
-  answer += y[2];
-} else {
-  answer += y[0];
-}
-
-console.log(answer);
+console.log(solution(input));
