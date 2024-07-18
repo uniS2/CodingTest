32193e7d61b66e166d6eaf36a304496bef8b39ab
uniS2 <s2oie1010@gmail.com>
2024-07-18 23:04:05 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 436 ms, Memory: 58576 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin', 'utf8').trim().split('\n');
+
+function enter(data) {
+  const n = input.shift();
+  data = data.map(d => d.trim().split(' '));
+  let enter_list = new Set();
+
+  for (let i = 0; i < n; i++) {
+    if (data[i][1] === 'enter') {
+      enter_list.add(data[i][0])
+    } else {
+      enter_list.delete(data[i][0])
+    }
+  }
+  return [...enter_list].sort((a, b) => a > b ? -1 : 1).join('\n');
+}
+
+console.log(enter(input));
