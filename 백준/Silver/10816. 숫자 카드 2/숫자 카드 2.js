74b81dd18a73dee617c48555334b47810e17a85d
uniS2 <s2oie1010@gmail.com>
2024-07-21 23:50:20 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 856 ms, Memory: 150136 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
@@ -2,14 +2,12 @@ const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin', 'utf8').trim().split('\n');
 
 function count(data) {
-  const M = data[0];
-  const N = data[2];
   const have_list = data[1].split(' ');
   const card_list = data[3].split(' ');
   let hash = new Map();
   let answer = [];
 
-  for (let i = 0; i < M; i++) {
+  for (let i = 0; i < data[0]; i++) {
     if (hash.has(have_list[i])) {
       hash.set(have_list[i], hash.get(have_list[i]) + 1);
     } else {
@@ -17,7 +15,7 @@ function count(data) {
     }
   }
 
-  for (let i = 0; i < N; i++) {
+  for (let i = 0; i < data[2]; i++) {
     if (hash.has(card_list[i])) {
       answer.push(hash.get(card_list[i]));
     } else {
