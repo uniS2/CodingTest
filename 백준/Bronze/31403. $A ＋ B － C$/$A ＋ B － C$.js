4bb907aeeff10221990d92c2b92e44272f54e703
uniS2 <s2oie1010@gmail.com>
2024-07-22 13:38:30 +0900

    [Bronze IV] Title: $A + B - C$, Time: 120 ms, Memory: 9568 KB -BaekjoonHub

--- 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.js
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.js
@@ -2,8 +2,9 @@ const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin', 'utf8').trim().split('\n').map(Number);
 
 function math(num) {
-  let answer = [num[0] + num[1] - num[2]];
-  answer.push('' + num[0] + num[1] - num[2]);
-  return answer.join('\n');
+  let answer = num[0] + num[1] - num[2];
+  answer += '\n';
+  answer += '' + num[0] + num[1] - num[2];
+  return answer;
 }
 console.log(math(input));
