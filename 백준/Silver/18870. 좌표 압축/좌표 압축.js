b62d00145b84e074e3c62e98076d7a57bffc9449
uniS2 <s2oie1010@gmail.com>
2024-07-29 23:44:44 +0900

    [Silver II] Title: 좌표 압축, Time: 1836 ms, Memory: 369320 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.js
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.js
@@ -1,16 +1,13 @@
 const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin', 'utf8').trim().split('\n');
 
-function press(input) {
-  let x = input[1].split(' ');
-  const xList = [...new Set(x)].sort((a, b) => +a - +b);
-  const xMap = new Map(xList.map((v, i) => [v, i]));
-  let output = '';
+function solution(input) {
+  let positions = input[1].split(' ').map(Number);
+  const unique = [...new Set(positions)].sort((a, b) => a - b);
+  const uniqueMap = new Map(unique.map((p, i) => [p, i]));
 
-  for (let i = 0; i < input[0]; i++) {
-    output += xMap.get(x[i]) + ' ';
-  }
-  return output;
+  const result = positions.map(p => uniqueMap.get(p));
+  return result.join(' ');
 }
 
-console.log(press(input));
+console.log(solution(input));
