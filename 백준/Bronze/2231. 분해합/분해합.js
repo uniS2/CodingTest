7febf1a3317fc2b33efacfc5b3b207707c544c3f
uniS2 <s2oie1010@gmail.com>
2024-08-04 23:41:01 +0900

    [Bronze II] Title: 분해합, Time: 100 ms, Memory: 9612 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.js
+++ 백준/Bronze/2231. 분해합/분해합.js
@@ -1,7 +1,13 @@
 const input = require('fs').readFileSync('/dev/stdin', 'utf8').trim();
 
 function solution(n) {
-  for (let i = 1; i < n; i++) {
+  let i = 1;
+
+  if (n > 27) {
+    i = n - n.length * 9;
+  }
+    
+  for (i; i < n; i++) {
     const arr = [...String(i)].map(Number);
     if (Number(n) === i + arr.reduce((a, b) => a + b, 0)) {
       return i;
