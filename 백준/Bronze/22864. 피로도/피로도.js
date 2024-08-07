911bde7606a3db8a1e151ffa9a3e0b6ddacaf0d9
uniS2 <s2oie1010@gmail.com>
2024-08-07 23:33:13 +0900

    [Bronze II] Title: 피로도, Time: 128 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/22864. 피로도/피로도.js
@@ -0,0 +1,20 @@
+const [a, b, c, m] = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split(' ').map(Number);
+
+function solution(a, b, c, m) {
+  let fatigue = 0;
+  let max_work = 0;
+
+  for (let time = 0; time < 24; time++) {
+    if (fatigue < m && fatigue + a <= m) {
+      fatigue += a;
+      max_work += b;
+    } else {
+      fatigue -= c;
+      if (fatigue < 0) fatigue = 0;
+    }
+  }
+
+  return max_work;
+}
+
+console.log(solution(a, b, c, m));
